2af09e97996abdf4524213ffef3c50637b1bc873
Wwjka <w.k.durajczyk@gmail.com>
2025-03-27 12:54:33 +0100

    loadImage

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,76 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button5 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(32, 117);
+            this.button1.Location = new System.Drawing.Point(197, 390);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "loadImage()";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(32, 154);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(127, 26);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "rotateImage90()";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(32, 187);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(127, 36);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "rotateImage180()";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(32, 230);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(127, 32);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "invertColors()";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button5
+            // imageList1
             // 
-            this.button5.Location = new System.Drawing.Point(32, 268);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(104, 28);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "onlyGreen()";
-            this.button5.UseVisualStyleBackColor = true;
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(329, 23);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(182, 398);
+            this.pbImage.TabIndex = 5;
+            this.pbImage.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pbImage);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Main Window";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -105,11 +82,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pbImage;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,7 @@ namespace pw4
 {
     public partial class Form1: Form
     {
+        private Bitmap originalBitmap;
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,19 @@ namespace pw4
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Bitmap files (*.bmp)|*.bmp";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    originalBitmap = new Bitmap(ofd.FileName);
+                    pbImage.Image = originalBitmap;
+                }
+            }
         }
     }
 }
